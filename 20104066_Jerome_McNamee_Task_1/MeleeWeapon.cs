ac49934a6db75a5a2046985a51114253cfb18f27
Jerome-McNamee <70902499+Jerome-McNamee@users.noreply.github.com>
2020-11-04 10:38:28 +0200

    Q.2.1 & 2.2

---
+++ 20104066_Jerome_McNamee_Task_1/MeleeWeapon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20104066_Jerome_McNamee_Task_1
+{
+    class MeleeWeapon: Weapon
+    {
+        public MeleeWeapon(MeleeWeaponType meleeWeaponType, int x = 0, int y = 0): base(x, y)
+        {
+            if(meleeWeaponType == MeleeWeaponType.DAGGER)
+            {
+                durability = 10;
+                damage = 3;
+                cost = 3;
+                name = "Dagger";
+            }
+
+            else if (meleeWeaponType == MeleeWeaponType.LONGSWORD)
+            {
+                durability = 6;
+                damage = 4;
+                cost = 5;
+                name = "LongSword";
+            }
+            range = 1;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public enum MeleeWeaponType
+        {
+            DAGGER,
+            LONGSWORD
+        }
+    }
+}
---
+++ 20104066_Jerome_McNamee_Task_1/Weapon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20104066_Jerome_McNamee_Task_1
+{
+    abstract class Weapon: Item
+    {
+        protected int damage;
+        protected int range;
+        protected int durability;
+        protected int cost;
+        protected string name;
+
+        public int Damage 
+        {
+            get
+            {
+                return damage;
+            }
+        }
+
+        public int Range
+        {
+            get
+            {
+                return range;
+            }
+        }
+        public int Durability
+        {
+            get
+            {
+                return durability;
+            }
+        }
+        public int Cost
+        {
+            get
+            {
+                return cost;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+
+
+        public Weapon (int x = 0, int y = 0): base(x, y)
+        {
+            this.type = TileType.WEAPON;
+        }
+    }
+}
